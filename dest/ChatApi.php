2f5b245aa8d5dfd669a0d14cd05b0ba6a934ea86
ryu s <aicreate.tatsuyoshi@gmail.com>
2023-03-15 09:08:45 +0900

    feat 確認おk

--- dest/ChatApi.php
+++ dest/ChatApi.php
@@ -8,16 +8,30 @@ $dotenv = Dotenv::createImmutable(TOKENPATH);
 $dotenv->load();
 $token = $_ENV['RPA_TOKEN'];
 $today = Carbon::now();
-$start_of_month = Carbon::now()->startOfMonth();
-if($start_of_month == $today){
-  $accout_ids = [2157434,2458725,3056863,4139020,7613810];
-} else {
-  $accout_ids = [2157415,3324425,2447973,4205422,6632060];
+$account_ids = [];
+if($today->day == 1){
+  echo '1日';
+  $account_ids = [2157415,3324425,2447973,4205422,6632060];
+} else if($today->day == 15){
+  echo '15日';
+  $account_ids = [2157434,2458725,3056863,4139020,7613810];
 }
+// if($today->hour == 10){
+//   echo '10時';
+//   $account_ids = [2157415,3324425,2447973,4205422,6632060];
+// } else if($now->hour == 11){
+//   echo '15日';
+//   $account_ids = [2157434,2458725,3056863,4139020,7613810];
+// }
+/**
+ * 自分の部屋
+ */
+// $roomid = 61814905;
+
 $roomid = 242709998;
 $url = 'https://api.chatwork.com/v2/rooms/'.$roomid.'/messages';
 $message = '[hr]';
-foreach($accout_ids as $accout_id){
+foreach($account_ids as $accout_id){
   $message.= '[To:'.$accout_id.'][pname:'.$accout_id.']さん'."\n";
 }
 $message.= 'Instagram、Twitterにて'."\n";
